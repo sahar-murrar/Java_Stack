1cd25dacacd9d9fa3f5ac586ef85c901564f122e
sahar_murrar <murrarsahar@gmail.com>
2021-06-09 00:46:09 +0300

    My First Java Program

---
+++ JavaFun/First Java Program/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram{
+    public static void main(String[] args) {
+        System.out.println("My Name is Sahar Murrar");
+	System.out.println("I am 22 Years Old");
+	System.out.println("My home twon is Beit Doqque, Ramallah");
+    }
+}
