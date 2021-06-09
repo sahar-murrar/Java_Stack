19b71ca20f06aad0cc023e76388f26c5ab2bfb44
sahar_murrar <murrarsahar@gmail.com>
2021-06-09 12:11:25 +0300

    FizzBuzz Assignment/java stack

---
+++ JavaFun/FizzBuzz/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String [] args){
+        FizzBuzz f = new FizzBuzz();
+        String result= f.fizzBuzz(15);
+        System.out.println(result);
+    }
+
+}
