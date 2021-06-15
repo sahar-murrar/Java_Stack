66d78d695c156763340103a796fb161234494858
sahar_murrar <murrarsahar@gmail.com>
2021-06-15 13:26:16 +0300

    String Assignment/ first spring boot project in java

---
+++ Java Spring/StringAssignment/src/test/java/com/codingdojo/stringassignment/StringAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.stringassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
